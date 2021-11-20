80291f218cd265d152bca733c32a62525962ba68
Marcos Riani <marcosriani@hotmail.com>
2021-11-20 17:25:36 +0000

    Ball bouncing when hitting the walls of the game.

--- src/script.js
+++ src/script.js
@@ -118,8 +118,8 @@ class Player {
     this.x = 100;
     this.y = 300;
     this.radius = 30;
-    this.speedX = 150;
-    this.speedY = 150;
+    this.speedX = 2;
+    this.speedY = 2;
   }
 
   draw() {
@@ -132,74 +132,76 @@ class Player {
   }
 
   detectWalls() {
-    // Left wall
-    if (this.x < 0) {
-      this.speedX--;
-      this.speedY--;
+    //   Left wall
+    if (this.x - this.radius < 0) {
+      console.log('left');
+      this.speedX++;
     }
 
     //   Right wall
-    if (this.x + this.w > canvas.width) {
-      this.x = canvas.width - this.w;
+    if (this.x + this.radius > CANVAS_WIDTH) {
+      console.log('righ');
+      this.speedX--;
     }
 
     //   Top wall
     if (this.y < 0 + this.radius) {
-      this.speedX--;
       this.speedY--;
     }
 
     //  Bottom wall
     if (this.y + this.radius + 20 > canvas.height) {
-      this.speedX++;
       this.speedY++;
     }
   }
 
-  moveLeft() {
-    this.x -= this.speedX;
-  }
+  //   moveLeft() {
+  //     this.x -= this.speedX;
+  //   }
 
-  moveRight() {
-    this.x += this.speedX;
-  }
+  //   moveRight() {
+  //     this.x += this.speedX;
+  //   }
 
-  moveUp() {
-    this.y -= this.speedY;
-  }
+  //   moveUp() {
+  //     if (this.y > 0 + this.radius) {
+  //       this.y -= this.speedY;
+  //     }
+  //   }
 
-  moveDown() {
-    this.y += this.speedY;
-  }
+  //   moveDown() {
+  //     if (this.y < CANVAS_HEIGHT - this.radius) {
+  //       this.y += this.speedY;
+  //     }
+  //   }
 
   update() {
     this.detectWalls();
-    // this.y -= this.speedY;
+    this.y -= this.speedY;
+    this.x += this.speedX;
   }
 }
 
 const player = new Player();
 
-window.addEventListener('keydown', (event) => {
-  if (event.key === 'ArrowLeft') {
-    console.log('left');
+// window.addEventListener('keydown', (event) => {
+//   if (event.key === 'ArrowLeft') {
+//     player.moveLeft();
+//   }
 
-    player.moveLeft();
-  }
+//   if (event.key === 'ArrowRight') {
+//     player.moveRight();
+//   }
 
-  if (event.key === 'ArrowRight') {
-    player.moveRight();
-  }
+//   if (event.key === 'ArrowUp') {
+//     console.log('up');
 
-  if (event.key === 'ArrowUp') {
-    console.log('up');
-
-    player.moveUp();
-  }
-  if (event.key === 'ArrowDown') {
-    player.moveDown();
-  }
-});
+//     player.moveUp();
+//   }
+//   if (event.key === 'ArrowDown') {
+//     player.moveDown();
+//   }
+// });
 
 const animation = () => {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
