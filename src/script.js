adec2aa5718b1e43ce1e6373d223be1cdcd02242
Marcos Riani <marcosriani@hotmail.com>
2021-11-20 15:31:56 +0000

    Adding ground static ground image to the canvas world

--- src/script.js
+++ src/script.js
@@ -9,31 +9,13 @@ const cloudImage1 = document.querySelector('#cloud1');
 const cloudImage2 = document.querySelector('#cloud2');
 const birdImage1 = document.querySelector('#bird1');
 const birdImage2 = document.querySelector('#bird2');
-
-class Tree {
-  constructor(canvasW, canvasH) {
-    this.w = Math.floor(Math.random() * (60 - 25 + 1) + 25);
-    this.h = Math.floor(Math.random() * (250 - 45 + 1) + 45);
-    this.speed = Math.random() * 10 + 1;
-    this.x = canvasW;
-    this.y = canvasH;
-  }
-
-  draw() {
-    ctx.fillStyle = this.speed > 3 ? '#e25822' : 'rgba(135, 166, 245, 0.303)';
-    ctx.fillRect(this.x, this.y - this.h, this.w, this.h);
-  }
-
-  update() {
-    this.x -= this.speed;
-  }
-}
+const ground = document.querySelector('#ground');
 
 class FlyingImages {
   constructor(cloudImg, maxW, maxH, minW, minH) {
     this.w = Math.floor(Math.random() * (maxW - minW + 1) + minW);
     this.h = Math.floor(Math.random() * (maxH - minH + 1) + minH);
-    this.speed = 1;
+    this.speed = Math.floor(Math.random() * (3 - 2 + 1) + 2);
     this.x = 600;
     this.y = Math.floor(Math.random() * (150 - 0 + 1) + 0);
     this.cloudImg = cloudImg;
@@ -48,26 +30,7 @@ class FlyingImages {
   }
 }
 
-const arrayOfTrees = [];
 const arrayFlyingItems = [];
-
-const treeHandler = () => {
-  if (gameFrame % 50 === 0) {
-    arrayOfTrees.push(new Tree(CANVAS_WIDTH, CANVAS_HEIGHT));
-  }
-
-  arrayOfTrees.forEach((treeItem) => {
-    treeItem.draw();
-    treeItem.update();
-  });
-
-  //   Check if the tree has past the canvas border and removes it
-  arrayOfTrees.forEach((treeItem, index) => {
-    if (treeItem.x < -treeItem.w) {
-      arrayOfTrees.splice(index, 1);
-    }
-  });
-};
 
 const flyingImageHandler = (images, imageQuantity) => {
   // Make sure random images are displayed starting at index 0
@@ -96,9 +59,12 @@ const flyingImageHandler = (images, imageQuantity) => {
   });
 };
 
-const backgroundLayer1 = new Image();
+// BACKGROUND LAYER
 let gameSpeed = 1;
+const backgroundLayer1 = new Image();
 backgroundLayer1.src = '../img/mid_ground_cloud_2.png';
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = '../img/ground.png';
 
 class Layer {
   constructor(image, speedModifier) {
@@ -133,12 +99,26 @@ class Layer {
 
 const layer1 = new Layer(backgroundLayer1, 0.5);
 
+// Building the ground
+class Ground {
+  constructor() {
+    this.x = 0;
+    this.y = 320;
+  }
+
+  draw() {
+    ctx.drawImage(ground, this.x, this.y, 600, 80);
+  }
+}
+
+const buildGround = new Ground();
+
 const animation = () => {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  layer1.update();
   layer1.draw();
-  treeHandler();
+  layer1.update();
   flyingImageHandler([cloudImage1, cloudImage2, bird1, bird2], 3);
+  buildGround.draw();
   gameFrame += 1;
   requestAnimationFrame(animation);
 };
